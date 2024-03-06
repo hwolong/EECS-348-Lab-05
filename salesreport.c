51c29f169e8ab229e0e194a0676277440006d5c6
Hunter Long <hwolong@gmail.com>
2024-03-06 14:04:24 -0600

    code update
    added minimum and maximum functions as well as code for printing various values

--- salesreport.c
+++ salesreport.c
@@ -1,12 +1,66 @@
 #include <stdio.h>
 
+int minimum(float array[]) {
+	int min;
+	int minIndex;
+	for (int i = 0; i < 12; i++) {
+		if (array[i] < min) {
+			min = array[i];
+			minIndex = i;
+		}
+	}
+	return minIndex;
+}
+
+int maximum(float array[]) {
+	int max;
+	int maxIndex;
+	for (int i = 0; i < 12; i++) {
+		if (array[i] > max) {
+			max = array[i];
+			maxIndex = i;
+		}
+	}
+	return maxIndex;
+}
+
+float movingAverage(float array[]) {
+	char months[][12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};	
+    float averageList[7] = {0};
+    for (int i = 0; i <= 6; i++) {
+        float avg = 0;
+        for (int j = 0; i <= 5; i++) {
+            avg += array[j+i];
+        }
+        avg /= 6;
+        averageList[i] = avg;
+    }
+    for (int i = 0; i <= 6; i++) {
+		printf("%s - %s: %f\n", months[i], months[i+6], averageList[i]);
+	}
+}
+
 int main() {
-	int sales[12];
-	char months[][9] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	float sales[12] = {0};
+	char months[][12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	for (int i=0; i<12; i++) {
-		scanf("%d\n", &sales[i]);
+		scanf("%f", &sales[i]);
 	}
 
-	printf("Monthly sales report for 2022:\n");
-	printf("January\t%d", sales[0])
+	printf("Monthly sales report for 2022:\n\n");
+	for (int i = 0; i < 12; i++) {
+		printf("%s: $%.2f\n", months[i], sales[i]);
+	}
+	int min = minimum(sales);
+	int max = maximum(sales);
+	printf("Minimum sales: $%.2f (%s)\n", sales[min], months[min]);
+	printf("Maximum sales: $%.2f (%s)\n", sales[max], months[max]);
+	float avg = 0.0;
+	for (int i = 0; i < 12; i++) {
+		avg += sales[i];
+	}
+	avg /= 12;
+	printf("Average sales: %f\n", avg);
+	printf("Six-Month Moving Average Report:\n");
+	movingAverage(sales);
 }
