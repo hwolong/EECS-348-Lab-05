cfddbff9ad63b7ad4ce09240b5be044693585242
Hunter Long <hwolong@gmail.com>
2024-03-06 13:25:22 -0600

    Update salesreport.c
    actually put everything in a main() function this time

--- salesreport.c
+++ salesreport.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int sales[12];
-for (int i=0; i<12; i++) {
-    scanf("%d\n", &sales[i]);
+int main() {
+	int sales[12];
+	char months[][9] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	for (int i=0; i<12; i++) {
+		scanf("%d\n", &sales[i]);
+	}
+
+	printf("Monthly sales report for 2022:\n");
+	printf("January\t%d", sales[0])
 }
